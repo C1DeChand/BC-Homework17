5d865ee3f2fd7b28c92b8fecf8febde6fae99235
C1DeChand <C1DeChand@gmail.com>
2020-07-02 12:11:21 -0700

    updated for heroku deploy

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.post("/submit", ({ body }, res) => {
   User.create(body)
